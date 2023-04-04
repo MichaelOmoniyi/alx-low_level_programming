fca397fc43a9f08596b16035cf7f987c6b81e3da
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-04-04 04:14:55 +0100

    Update file 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,18 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+int len;
+len = 0;
+
+while (dest[len] != '\0')
+{
+len++;
+}
 
 for (i = 0; i < n; i++)
 {
-*(dest + 1) = *(src + 1);
+len =+ 1;
+*(dest + len) = *(src + 1);
 }
 
 return (dest);
