36284a7be0ed38192215fdb49aa6a3e5606b01b3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-04-13 18:36:25 +0100

    Update files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: Size of memory to be allocate.
+ * Return: Returns pointer to array.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+
+if (p == NULL)
+{
+exit(98);
+}
+
+return(p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef
+#define
+
+void *malloc_checked(unsigned int b);
+
+#endif
