ceba52847d9f7e0e8ed668ecc5c6bacc0b5b5f94
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-04-21 12:42:17 +0100

    Update advance task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,32 +7,6 @@
  * Return: 0, when successful.
  */
 
-/**
- * op_codes - A function that print the opcode of itself.
- * @bytes: Number of Bytes to be printed.
- * Return: 0, when successful.
- */
-
-/**
- * int op_codes(int bytes)
-{
-char *main_ptr = (char *)op_codes;
-
-int i;
-
-for (i = 0; i < bytes; i++)
-{
-if (i == bytes - 1)
-{
-printf("%02hhx\n", main_ptr[i]);
-break;
-}
-printf("%02hhx ", main_ptr[i]);
-}
-return (0);
-}
-**/
-
 int main(int argc, char *argv[])
 {
 int bytes, i;
