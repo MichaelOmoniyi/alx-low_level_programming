a880cf485d956074da377e988334eb54a77f08c1
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-04-28 15:30:59 +0100

    Update header file

--- 0x12-singly_linked_lists/trailling_whitespace.sh
+++ 0x12-singly_linked_lists/trailling_whitespace.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-vim file -c "%s/\s\+$//" -c "wq"
+#Checks If file name is provided as argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+file="$1"
+
+#Checks if file exits
+if [ ! -e "$file" ]; then
+    echo "Error: File '$file' does not exit"
+    exit 1
+fi
+
+#Remove trailing whitespace using sed command
+sed -i 's/\s\+$//' "$file"
+
+echo "Trailing whitespace removed from '$file'"
