d0f2b72f4797950b172fd33ab2627e28e8a92d58
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-05-04 02:35:12 +0100

    Update '9-insert_nodeint.c'

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *new_node;
-listint_t *temp = *head;
+listint_t *new_node, *temp = *head;
 unsigned int node;
 
 new_node = malloc(sizeof(listint_t));
