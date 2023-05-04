11d6583d4df1dc3eca4d04ab5b22765160aa815a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-05-04 02:45:04 +0100

    Update '9-insert_nodeint.c'

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  * Return: 1, if it suceeded, -1 i it fails.
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *new_node, *temp = *head;
 unsigned int node;
@@ -25,7 +25,7 @@ return (NULL);
 new_node->n = n;
 
 if (idx == 0)
-
+{
 new_node->next = temp;
 *head = new_node;
 return (new_node);
