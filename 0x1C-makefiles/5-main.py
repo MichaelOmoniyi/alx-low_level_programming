5ae7f3fc55c7f73889a845a5ae3816f48acb9b4a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-10-26 13:13:17 +0000

    Update test file for 5-island_perimeter.py

--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -12,4 +12,41 @@ if __name__ == "__main__":
         [0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0]
     ]
-    print(island_perimeter(grid))
+    print("For the first grid")
+    print("Expected perimeter: 12")
+    print("Perimeter returned: ", island_perimeter(grid))
+    print("-------------------------------------------")
+
+    grid_2 = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
+
+    print("For the second grid")
+    print("Expected answer: 16")
+    print("Perimeter returned: ", island_perimeter(grid_2))
+
+    grid_3 = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 1, 1, 1]
+    ]
+    print("For the third grid")
+    print("Expected answer: 20")
+    print("Perimeter returned: ", island_perimeter(grid_3))
+
+    grid_4 = [
+        [0, 1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1, 1],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0]
+    ]
+    print("For the forth grid")
+    print("Expected answer: 28")
+    print("Perimeter returned: ", island_perimeter(grid_4))
+
+
